adc5613a38e620dea224fd10ec1afcdd740ec5c3
Venkey0467 <s.venkataramana0353@gmail.com>
2023-05-12 04:26:56 -0700

    User 4 is added

---
+++ src/test/java/Git/AddUser4.java
@@ -0,0 +1,58 @@
+package Git;
+
+import static io.restassured.RestAssured.*;
+import org.testng.annotations.Test;
+
+public class AddUser4 {
+
+	@Test
+	public void CreateUser1()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"boss\",\r\n"
+					+ "    \"role\": \"sm\",\r\n"
+					+ "    \"age\": 31,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+		.when()
+			.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
+	
+	@Test
+	public void CreateUser2()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"venkey\",\r\n"
+					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"age\": 28,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+		.when()
+			.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
+
+	@Test
+	public void CreateUser3()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"yuvan\",\r\n"
+					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"age\": 22,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+		.when()
+			.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
+}
